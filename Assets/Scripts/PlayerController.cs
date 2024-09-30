7feca5159788d11e70e1b127915fec81ee80e215
jmalekx <151661788+jmalekx@users.noreply.github.com>
2024-09-30 10:32:29 +0100

    2.2.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,25 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour {
     public Vector2 moveValue;
     public float speed;
+    private int count;
 
+    void Start() {
+        count = 0;
+    }
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>();
     }
 
-    private void FixedUpdate() {
+    void FixedUpdate() {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
 
         GetComponent<Rigidbody>().AddForce(movement*speed*Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter (Collider other) { 
+        if (other.gameObject.tag == "Pickup") { 
+            other.gameObject.SetActive(false);
+            count += 1;
+        }
     }
 }
