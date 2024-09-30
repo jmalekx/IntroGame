44596326a9c1d45d576c4a6e48e6a9eb748bb5ca
darenagos <103580328+darenagos@users.noreply.github.com>
2024-09-30 11:25:27 +0100

    3.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+//using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
-public class PlayerController : MonoBehaviour {
-    public Vector2 moveValue;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //public Vector2 moveValue;
     public float speed;
     private int count;
+    private int numPickups = 3; //number of pickups we have
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
 
-    void Start() {
+
+    void Start()
+    {
         count = 0;
+        winText.text = "";
+        SetCountText();
     }
-    void OnMove(InputValue value) {
-        moveValue = value.Get<Vector2>();
+    //void OnMove(InputValue value)
+    //{
+    //    moveValue = value.Get<Vector2>();
+    //}
+
+    void FixedUpdate()
+    {
+    //    Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+
+    //    GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
     }
 
-    void FixedUpdate() {
-        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
-
-        GetComponent<Rigidbody>().AddForce(movement*speed*Time.fixedDeltaTime);
-    }
-
-    void OnTriggerEnter (Collider other) { 
-        if (other.gameObject.tag == "PickUp") { 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
             other.gameObject.SetActive(false);
             count += 1;
+            SetCountText();
         }
+    }
+
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if (count >= numPickups)
+        {
+            winText.text = "You win!";
+        }
+
     }
 }
